3aa935707a5ec93c35da1f6810cf28337a200e7d
yogyy <yogimr261@gmail.com>
2024-10-30 18:28:15 +0700

    chore: add schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,6 +6,6 @@ import { Pool } from "pg";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
-const db = drizzle({ client: pool });
+const db = drizzle({ client: pool, schema });
 
 export { schema, db };
